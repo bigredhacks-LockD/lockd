63751de2db97f955abb7fc3aec215360afa56f92
TailaiYing <taiying9627@gmail.com>
2024-10-05 16:27:19 -0400

    applied some styles

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -2,13 +2,24 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Status from './(tabs)/status';
 import BreakIn from './(tabs)/break-in';
-// import Lock from './tabs/Lock';       // Import your Lock screen
 
 const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          borderTopWidth: 0, // Remove the top border
+          elevation: 0, // Remove the shadow on Android 
+        },
+        tabBarLabelStyle: {
+          fontSize: 16, // Adjust font size for labels
+          fontWeight: 'bold', // Optional: make the labels bold
+        },
+        tabBarShowLabel: true, // Set to false to hide labels
+      }}
+    >
       <Tab.Screen name="Status" component={Status} />
       <Tab.Screen name="Break In" component={BreakIn} />
       {/* <Tab.Screen name="Lock" component={Lock} /> */}
